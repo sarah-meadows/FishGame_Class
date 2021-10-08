bf4b05d3918d3a6fac67526c6de99f84d3ca2b63
TheBigStinky <ajc2943@gmail.com>
2021-10-08 10:49:28 -0400

    Fixed Imports

--- Assets/Radical Forge/Halftone Shaders/Demo/Scripts/ControlViewer.cs
+++ Assets/Radical Forge/Halftone Shaders/Demo/Scripts/ControlViewer.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Policy;
+using System.Security;
 using UnityEngine;
 
 public class ControlViewer : MonoBehaviour
