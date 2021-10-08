a0c8410191fadc00d3754a69a6269fbdfdf294e3
sarah-meadows <59574997+sarah-meadows@users.noreply.github.com>
2021-10-08 13:30:29 -0400

    Class Today_Week6
    In this it the changes for today.
    It includes:
    The code for the bobber behavior
    Reorganized / moved around / created folders:
    
    Note: Monica, I moved your solo edits of the menu code to "_soloWork/Monica"
    Also Note: Lona, you might have to resubmit your boat model. For some reason I didn't see it in the recent updates
    
    Note to everyone:
    PLEASE BE ORGANIZED!!!!

--- Assets/Scripts/bobberBehavior.cs
+++ Assets/Scripts/bobberBehavior.cs
@@ -11,34 +11,50 @@ public class bobberBehavior : MonoBehaviour
     public float curCord_X;
     public float curCord_Z;
 
-    float fishRange=500;
+    public float fishRange=500;
     float bobSpeed=2;
 
-    public bool tiltLeft, tiltRight, tiltUp, tiltDown;
+    bool tiltLeft, tiltRight, tiltUp, tiltDown;
 
     GameObject centerBobber;
     GameObject[] bounds;
 
     public float inGameRange;
-    //
     public float calibrateCord;
-    Vector2 fishTargetPos, curTargetPos;
 
-    float clockMax = 30;
-    float timeRemaining;
+    Vector3 fishTargetPos, curTargetPos;
+    public float distFromTarget, percentFromTarget;
+    
     Image clock;
+    float maxSeconds = 20f;
+    float timeRemaining;
 
-    bool gameCondition; //true win, false lose
+    bool winGame=false, loseGame=false;
 
-    public AudioSource beeper;
-    public AudioClip beepsound;
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume = 0.1f;
 
+    bool buzzTimerIsBusy;
+
+    /**
+Vector2 fishTargetPos, curTargetPos;
+
+float clockMax = 30;
+float timeRemaining;
+Image clock;
+
+bool gameCondition; //true win, false lose
+
+public AudioSource beeper;
+public AudioClip beepsound;
+*/
 
     // Start is called before the first frame update
     void Start()
     {
         //let's really randomize the results. We'll randomize the numbers 3 times. 
-        for(int i=0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             fishCord_X = Random.Range(-fishRange, fishRange);
             fishCord_Z = Random.Range(-fishRange, fishRange);
@@ -48,22 +64,21 @@ public class bobberBehavior : MonoBehaviour
         bounds = GameObject.FindGameObjectsWithTag("bobberLimit");
 
         inGameRange = Vector3.Distance(bounds[0].transform.position, centerBobber.transform.position);
-        
-        //
-
-        //This is making the ratio between the in game cords and the fishRange
         calibrateCord = inGameRange * (1 / fishRange);
-        
+
+
         //This is the ingame cordinates of the fish position
-        float newX = (fishCord_X * calibrateCord)+centerBobber.transform.position.x;
+        float newX = (fishCord_X * calibrateCord) + centerBobber.transform.position.x;
         float newY = (centerBobber.transform.position.y);
-        float newZ = (fishCord_Z * calibrateCord)+ centerBobber.transform.position.z;
+        float newZ = (fishCord_Z * calibrateCord) + centerBobber.transform.position.z;
+
         fishTargetPos = new Vector3(newX, newY, newZ);
 
+        this.transform.position = centerBobber.transform.position;
 
-        //This is the clock for the catch timer
         clock = GameObject.Find("Clock").GetComponent<Image>();
-        timeRemaining = clockMax;
+        timeRemaining = maxSeconds;
+    
     }
 
     // Update is called once per frame
@@ -88,68 +103,77 @@ public class bobberBehavior : MonoBehaviour
         if (tiltLeft) { curCord_X -= bobSpeed; }
 
 
-        ///We need to: 
-        ///set the position of bobber to the curPos equivalent to real Space 
-        ///So we need to make a ratioConversion value
-        ///set our bober to the converted value plus is starting pos
-        ///_We need to make a countdown timer
-        ///We need a condition so that 
-        ///if at the end of the timer if the fish didn't get away, then you win
-        ///if before or at the end of the timer the fish gets away, the you lose
-        ///_We need to emulate the buzzing that equates to proximity (lets use sound for now). 
-        ///We can make it so that the sound is fired 
-        ///then must wait for timer cycle to reset
-        ///the timer cycle is relitive to the percent away from target
-        ///if you're close the percent is low, thus the buzz will fire more often
-        ///_We need to make a fish can get away if you're not careful
-        ///
-
-
-
         //This will translate the bobber accordingly
-        //What might these following floats are supposed to be? 
-        float curX = 1;
-        float curY = 1;
-        float curZ = 1;        
-        this.transform.position = new Vector3(curX, curY, curZ);
+        float curX = (curCord_X * calibrateCord) + centerBobber.transform.position.x;
+        float curY = centerBobber.transform.position.y;
+        float curZ = (curCord_Z * calibrateCord) + centerBobber.transform.position.z;        
+        
+        curTargetPos = new Vector3(curX, curY, curZ);
+        this.transform.position = curTargetPos;
 
-        
-        
-        //This will get the time
+        //getting the distance from goal and percentage away from goal
+        distFromTarget = Vector3.Distance(fishTargetPos, curTargetPos);
+        percentFromTarget = (distFromTarget / calibrateCord) / (fishRange * 2);
+
+
+        //setting up the buzzing indicator
+        if (!buzzTimerIsBusy){StartCoroutine(buzzIndicate());}
+
+
+
+        //Setting up our timer and win conditions
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
-            clock.fillAmount = timeRemaining / clockMax;
+            clock.fillAmount = timeRemaining / maxSeconds;
         }
-
-        //This will determine the win/lose scenarios
-        if (timeRemaining <= 0)
+        if(timeRemaining <= 0)
         {
-            print("there must be some sort of win/lose condition");
-
-            //if ("The losing Condition") { gameCondition = false; }
-            //if ("The Winning Condition") { gameCondition = true; }
+            //set our win/lose conditions
+            //if(our check and balance is true){winGame=true; }
+            //if(our check and balance is false){loseGame=true; }
         }
 
-        if (!gameCondition)
+        if (winGame)
         {
-            print("you lose");
+            //do a prompt and continue to next scene
         }
-        if (gameCondition)
+
+        if (loseGame)
         {
-            print("you win");
+            //do a prompt and allow fishing again
         }
 
 
-        //audioSource.PlayOneShot(clip, volume);
 
     }
+
+    IEnumerator buzzIndicate()
+    {
+        buzzTimerIsBusy = true;
+        yield return new WaitForSeconds(percentFromTarget);
+        audioSource.PlayOneShot(clip, volume);
+        buzzTimerIsBusy = false;
+    }
+
     private void OnDrawGizmos()
     {
-
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(fishTargetPos, 1);
-
     }
-
 }
+///We need to: 
+///set the position of bobber to the curPos equivalent to real Space 
+///
+///_We need to make a countdown timer
+///We need figure out how the fish can get away
+///if at the end of the timer if the fish didn't get away, then you win
+///if before or at the end of the timer the fish gets away, the you lose
+///_We need to emulate the buzzing that equates to proximity (lets use sound for now). 
+///We can make it so that the sound is fired 
+///then must wait for timer cycle to reset
+///the timer cycle is relitive to the percent away from target
+///if you're close the percent is low, thus the buzz will fire more often
+///_We need to make a fish can get away if you're not careful
+///
+
--- Assets/Scripts/fishConvo.cs
+++ Assets/Scripts/fishConvo.cs
@@ -8,7 +8,7 @@ public class fishConvo : MonoBehaviour
 {
     int fishType;
     public int convoCounter;
-    public int convoPossibility;
+    public int dialogueBranch;
 
     int choice;
 
@@ -17,79 +17,99 @@ public class fishConvo : MonoBehaviour
     Text fishStatement;
     Text responseA, responseB, responseC;
 
-    Button btnA, btnB, btnC;
+    Button btnA, btnB; 
+    //, btnC;
+
     // Start is called before the first frame update
     void Start()
     {
+        ///Things to do
+        ///---you need a way of detecting THE END OF CONVO
+        ///---you need a way to determine if you've WON/LOST
+        ///---you'll need add happy meter
+        ///---if the fishtype value is equal to x, the model will be x
+        ///---determine instantiate model position
+
+
         fishStatement = GameObject.Find("FishText/Text").GetComponent<Text>();
 
         responseA = GameObject.Find("responseA/Text").GetComponent<Text>();
         responseB = GameObject.Find("responseB/Text").GetComponent<Text>();
-        responseC = GameObject.Find("responseC/Text").GetComponent<Text>();
+        //responseC = GameObject.Find("responseC/Text").GetComponent<Text>();
 
         btnA = GameObject.Find("responseA").GetComponent<Button>();
         btnB = GameObject.Find("responseB").GetComponent<Button>();
-        btnC = GameObject.Find("responseC").GetComponent<Button>();
+        //btnC = GameObject.Find("responseC").GetComponent<Button>();
 
         btnA.onClick.AddListener(answerOptions);
         btnB.onClick.AddListener(answerOptions);
-        btnC.onClick.AddListener(answerOptions);
+        //btnC.onClick.AddListener(answerOptions);
+
+        // if (gameObject.activeInHierarchy == false) { gameObject.SetActive(true); }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (convoCounter == 3) { convoCounter = 0; }
-
+        //note the fishtype is equal to whatever fish it is
+        //alternitivly, you could do a string value instead of number
 
         if (fishType == 0)
         {
             //////////////////////////////////
             if (convoCounter == 0)
             {
-                if (convoPossibility==0)
+                if (dialogueBranch==0)
                 {
                     fishStatement.text = "example phrase 1";
 
                     responseA.text = "this is option A goes to possible 0";
-                    responseB.text = "this is option B goes to possible 0";
-                    responseC.text = "this is option C goes to possible 1";
+                    responseB.text = "this is option B goes to possible 1";
+                    //responseC.text = "this is option C goes to possible 1";
+                   
+                    responseB.gameObject.SetActive(true);
 
                 }
             }
             //////////////////////////////////
             if (convoCounter == 1)
             {
-                if (convoPossibility==0)
+                if (dialogueBranch==0)
                 {
-                    fishStatement.text = "1. example phrase 2 and one possible branch. This will loop to the beginning";
+                    fishStatement.text = "banana";
 
                     responseA.text = "this is option A1";
                     responseB.text = "this is option B1";
-                    responseC.text = "this is option C1";
 
                 }
-                if (convoPossibility==1)
+                if (dialogueBranch==1)
                 {
-                    fishStatement.text = "2. example phrase 2 and the second possible branch. This will loop to the beginning";
+                    fishStatement.text = "tomato";
 
                     responseA.text = "this is option A2";
-                    responseB.text = "this is option B2";
-                    responseC.text = "this is option C2";
+                    responseB.text = "This will result in loop";
 
                 }
             }
             //////////////////////////////////
             if (convoCounter == 2)
             {
-                if (convoPossibility==0)
+                if (dialogueBranch==0)
                 {
-                    fishStatement.text = "and now it loops";
+                    fishStatement.text = "lalala";
 
                     responseA.text = "option A";
                     responseB.text = "option B";
-                    responseC.text = "option C";
 
+                }
+                if (dialogueBranch==1)
+                {
+                    fishStatement.text = "WHO ARE YOU";
+
+                    responseA.text = "Bob";
+                   // responseB.text = "option B";
+
+                    responseB.gameObject.SetActive(false);
                 }
             }
             //////////////////////////////////
@@ -102,44 +122,45 @@ public class fishConvo : MonoBehaviour
 
         if (nameOfButton == "responseA") { choice = 0; }
         if (nameOfButton == "responseB") { choice = 1; }
-        if (nameOfButton == "responseC") { choice = 2; }
+       // if (nameOfButton == "responseC") { choice = 2; }
 
         //////////////////////////////////
         if (convoCounter == 0)
         {
-            if (convoPossibility == 0)
+            if (dialogueBranch == 0)
             {
-                if (choice == 0) { happyPoints++; convoPossibility = 0; convoCounter++; return;}
-                if (choice == 1) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 2) { happyPoints--; convoPossibility = 1; convoCounter++; return; }
+                if (choice == 0) { happyPoints++; dialogueBranch = 0; convoCounter++; return;}
+                if (choice == 1) { happyPoints++; dialogueBranch = 1; convoCounter++; return; }
                 
             }            
         }
         //////////////////////////////////
         if (convoCounter == 1)
         {
-            if (convoPossibility == 0)
+            if (dialogueBranch == 0)
             {
-                if (choice == 0) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 1) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 2) { happyPoints--; convoPossibility = 0; convoCounter++; return; }
+                if (choice == 0) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
+                if (choice == 1) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
             }
 
-            if (convoPossibility == 1)
+            if (dialogueBranch == 1)
             {
-                if (choice == 0) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 1) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 2) { happyPoints--; convoPossibility = 0; convoCounter++; return; }
+                if (choice == 0) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
+                if (choice == 1) { happyPoints++; dialogueBranch = 1; convoCounter++; return; }
             }
         }
         //////////////////////////////////
         if (convoCounter == 2)
         {
-            if (convoPossibility == 0)
+            if (dialogueBranch == 0)
             {
-                if (choice == 0) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 1) { happyPoints++; convoPossibility = 0; convoCounter++; return; }
-                if (choice == 2) { happyPoints--; convoPossibility = 0; convoCounter++; return; }
+                if (choice == 0) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
+                if (choice == 1) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
+            }
+            if (dialogueBranch == 1)
+            {
+                if (choice == 0) { happyPoints++; dialogueBranch = 0; convoCounter=0; return; }
+                //if (choice == 1) { happyPoints++; dialogueBranch = 0; convoCounter++; return; }
             }
 
         }
