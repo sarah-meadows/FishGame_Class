9b9c57429eb62f74a7db20d077346a45188cf0c3
LON4 <ladelpho@pratt.edu>
2021-11-12 09:10:40 -0500

    Animations

---
+++ Assets/_SoloWork/Lona/triggerAnimations.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerAnimations : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+private void OnTriggerEnter(Collider other)
+  {
+   // if (other.gameobject.tag -- "Player") 
+   {
+      // this.gameObject.GetComponent<Animator>()
+   }
+  }
+}
