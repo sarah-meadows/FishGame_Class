8ec93186576df5c4c8d947859b4bc431b50f4dd6
sarah-meadows <59574997+sarah-meadows@users.noreply.github.com>
2021-11-12 15:42:04 -0500

    Class Edits for today
    Fixed some issues with exporting
    Should be the edits for everyone today

--- Assets/_SoloWork/Lona/triggerAnimations.cs
+++ Assets/_SoloWork/Lona/triggerAnimations.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class triggerAnimations : MonoBehaviour
 {
-    
+    Animator BobberAnim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BobberAnim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-private void OnTriggerEnter(Collider other)
-  {
-    if (other.gameObject.tag == "Player") 
-   {
-            print("triggered by player");
-            this.gameObject.GetComponent<Animator>();
 
-   }
-  }
+        ///READ PLEASE!!! Teacher note:
+        ///Let's consolidate code. 
+        ///See if you can integrade this code into the sccript fishingCasting script. 
+        ///It's good to have your code in one place. 
+        ///However, this is minimal priority. 
+        ///Let's entire game to function before cleaning up this code. 
+        ///
+        
+        //print(BobberAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        if (BobberAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            if (BobberAnim.gameObject.GetComponent<Animator>().GetInteger("animState") == 0)
+            {
+                BobberAnim.gameObject.GetComponent<Animator>().SetInteger("animState", 1);
+            }
+        }
+
+    }
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<Animator>().SetInteger("animState", 1);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.GetComponent<Animator>().SetInteger("animState", 0);
+        }
+    }
+    */
 }
