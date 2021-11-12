ff2c07eee14b06a66d8f22025b6bb513d453d260
LON4 <ladelpho@pratt.edu>
2021-11-12 13:07:36 -0500

    BobberAnimation
    Bobber animation

--- Assets/_SoloWork/Lona/triggerAnimations.cs
+++ Assets/_SoloWork/Lona/triggerAnimations.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class triggerAnimations : MonoBehaviour
 {
-    
+    public Animator BobberAnim ;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +14,27 @@ public class triggerAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        print(BobberAnim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        if(BobberAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >=1.0f){
+            if(this.gameObject.GetComponent<Animator>().GetInteger("animState")== 0){
+                this.gameObject.GetComponent<Animator>().SetInteger("animState", 1);
+            }
+        }
+        //print(BobberAnim.GetCurrentAnimatorStateInfo (0).IsName (current_state_name) );
+
     }
     
 private void OnTriggerEnter(Collider other)
   {
-   // if (other.gameobject.tag -- "Player") 
+    if (other.gameObject.tag == "Player") 
    {
-      // this.gameObject.GetComponent<Animator>()
+      this.gameObject.GetComponent<Animator>().SetInteger("animState", 1);
    }
   }
-}
+private void OnTriggerExit(Collider other)
+  {
+    if (other.gameObject.tag == "Player") 
+   {
+      this.gameObject.GetComponent<Animator>().SetInteger("animState", 0);
+   }
+  }}
